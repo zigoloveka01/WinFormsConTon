68d70a519554472194961037a8dd4f5fca7cafd6
zigoloveka01 <zigoloveka1@gmail.com>
2024-11-18 11:53:58 +0700

    Add project files.

---
+++ WinFormsConTon/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WinFormsConTon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(663, 390);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 48);
+            button1.TabIndex = 0;
+            button1.Text = "ปิดโปรแกรม";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(632, 248);
+            button2.Name = "button2";
+            button2.Size = new Size(144, 84);
+            button2.TabIndex = 1;
+            button2.Text = "เปิดไฟ";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            button2.MouseLeave += button2_MouseLeave;
+            button2.MouseHover += button2_MouseHover;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(637, 139);
+            button3.Name = "button3";
+            button3.Size = new Size(139, 89);
+            button3.TabIndex = 2;
+            button3.Text = "ปิดไฟ";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.Info;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ActiveCaptionText;
+            label1.Location = new Point(75, 49);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(503, 179);
+            label1.TabIndex = 3;
+            label1.Text = "มหาลัยราชภัฏอุดรธานี";
+            label1.Click += มหาวิทยาลัยราชภัฏอุดร;
+            label1.MouseLeave += label1_MouseLeave;
+            label1.MouseHover += label1_MouseHover;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(75, 351);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(197, 67);
+            textBox1.TabIndex = 4;
+            textBox1.Text = "ควยยยยยยยยยยยย";
+            textBox1.TextChanged += textBox1_TextChanged;
+            textBox1.DoubleClick += textBox1_DoubleClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "windowfromcontrol";
+            FormClosing += Form1_FormClosing;
+            Click += Form1_Click;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
---
+++ WinFormsConTon/Form1.cs
@@ -0,0 +1,103 @@
+﻿namespace WinFormsConTon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+
+            Random rm = new Random();
+            int r = rm.Next(0, 255);
+            int g = rm.Next(0, 255);
+            int b = rm.Next(0, 255);
+
+            this.BackColor = Color.FromArgb(r, g, b);
+            this.Text = r.ToString() + "," + g.ToString() + "," + b.ToString();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("ต้องการปิดจิงหรือไม่", "แนะใจนะ",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightYellow;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Black;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.LightSeaGreen;
+        }
+
+        private void button2_MouseLeave(object sender, EventArgs e)
+        {
+            button2.BackColor = Color.LightYellow;
+        }
+
+        private void มหาวิทยาลัยราชภัฏอุดร(object sender, EventArgs e)
+        {
+            if (label1.Text == "มหาลัยราชภัฏอุดรธานี")
+            {
+                label1.Text = "Udonthani Rajabhat University";
+            }
+            else
+            {
+                label1.Text = "มหาลัยราชภัฏอุดรธานี";
+            }
+        }
+
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.LightSeaGreen;
+            label1.Text = "Udonthani Rajabhat University";
+        }
+
+        private void label1_MouseLeave(object sender, EventArgs e)
+        {
+            label1.BackColor = Color.LightYellow;
+            label1.Text = "มหาลัยราชภัฏอุดรธานี";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+
+            Random rm = new Random();
+            int r = rm.Next(0, 255);
+            int g = rm.Next(0, 255);
+            int b = rm.Next(0, 255);
+            label1.ForeColor = Color.FromArgb(r, g, b);
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            //textBox1.Text = string.Empty;
+            //textBox1.Text = null;
+        }
+    }
+
+}
